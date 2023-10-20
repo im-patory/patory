e1c5baa9ba6d9aed8f9a8c874e1c2ea9bc401146
Lipraty <i@lonay.me>
2023-10-20 08:26:57 +0800

    add BasicEvent

---
+++ src/Events/Basic.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * patory
+ * Copyright (c) Lipraty 2023.
+ *
+ * PhpStorm, 10/20/2023
+ **/
+namespace Patory\Events;
+
+class Basic
+{
+    function _getEvents()
+    {
+
+    }
+}
